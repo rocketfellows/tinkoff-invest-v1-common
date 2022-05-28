a54af335cad35acdb425e22adf7b6c3ca0b72435
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-05-29 01:39:54 +0400

    Added exception.

---
+++ src/exceptions/factories/moneyValue/MoneyValueFactoryException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1Common\exceptions\factories\moneyValue;
+
+use rocketfellows\TinkoffInvestV1Common\exceptions\factories\BaseFactoryException;
+
+abstract class MoneyValueFactoryException extends BaseFactoryException
+{
+}
