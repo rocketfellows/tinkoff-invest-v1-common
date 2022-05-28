7928e3def687e6e1afc5282aa0127e96d3e8ede3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-05-29 01:46:08 +0400

    Added invalid currency field.

--- src/exceptions/factories/moneyValue/UnknownMoneyValueCurrencyException.php
+++ src/exceptions/factories/moneyValue/UnknownMoneyValueCurrencyException.php
@@ -2,6 +2,25 @@
 
 namespace rocketfellows\TinkoffInvestV1Common\exceptions\factories\moneyValue;
 
+use Throwable;
+
 class UnknownMoneyValueCurrencyException extends MoneyValueFactoryException
 {
+    private $currency;
+
+    public function __construct(
+        string $currency,
+        string $message = "",
+        int $code = 0,
+        Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+
+        $this->currency = $currency;
+    }
+
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
 }
