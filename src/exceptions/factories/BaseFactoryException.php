1b357d77873a7857677a2e1c0cfca5720dbcfa8f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-05-29 01:38:42 +0400

    Added exception.

---
+++ src/exceptions/factories/BaseFactoryException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1Common\exceptions\factories;
+
+use rocketfellows\TinkoffInvestV1Common\exceptions\BaseException;
+
+abstract class BaseFactoryException extends BaseException
+{
+}
