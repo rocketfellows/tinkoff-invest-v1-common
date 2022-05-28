c30493ff21f478a86a7354132b5dff538eba5dec
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-05-29 01:25:29 +0400

    Created factory test.

---
+++ tests/unit/factories/MoneyValueTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1Common\tests\unit\factories;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group factories
+ */
+class MoneyValueTest extends TestCase
+{
+}
