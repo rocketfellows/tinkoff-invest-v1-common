c2211f22a7bb658a479887045a57da445d945059
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-05-29 01:47:15 +0400

    Implemented factory.

--- src/factories/MoneyValueFactory.php
+++ src/factories/MoneyValueFactory.php
@@ -2,6 +2,24 @@
 
 namespace rocketfellows\TinkoffInvestV1Common\factories;
 
+use arslanimamutdinov\ISOStandard4217\Currency;
+use arslanimamutdinov\ISOStandard4217\ISO4217;
+use rocketfellows\TinkoffInvestV1Common\exceptions\factories\moneyValue\UnknownMoneyValueCurrencyException;
+use rocketfellows\TinkoffInvestV1Common\models\MoneyValue;
+
 class MoneyValueFactory
 {
+    /**
+     * @throws UnknownMoneyValueCurrencyException
+     */
+    public function create(string $currency, int $units, int $nano): MoneyValue
+    {
+        $isoCurrency = ISO4217::getByAlpha3($currency);
+
+        if (!$isoCurrency instanceof Currency) {
+            throw new UnknownMoneyValueCurrencyException($currency);
+        }
+
+        return (new MoneyValue($isoCurrency, $units, $nano));
+    }
 }
