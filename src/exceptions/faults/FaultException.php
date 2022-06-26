df0d12001578429b6a2ebe13f6e821445513b4bd
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-27 00:48:21 +0400

    Added abstract fault exception.

---
+++ src/exceptions/faults/FaultException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1Common\exceptions\faults;
+
+use rocketfellows\TinkoffInvestV1Common\exceptions\BaseException;
+
+abstract class FaultException extends BaseException
+{
+}
